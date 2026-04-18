c1ac61391224c7bef73224d4efbe1042d2666387
Kirtan Shah <s4108558@student.rmit.edu.au>
2026-04-18 13:22:01 +1000

    added upload functionality, need to work on displaying images now!!

--- flask-application/app.py
+++ flask-application/app.py
@@ -1,17 +1,39 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for, send_from_directory
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'flask-application\\uploads'
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'tiff']
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def hello_world():
-    return "Hello world!"
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/',  methods=['GET', 'POST'])
+def upload():
     if request.method == 'POST':
-        name = request.form['username']
-        return f"Hello {name}, POST request received."
-    return render_template('name.html')
+        if 'orthomosaic' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['orthomosaic']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+        
+    return render_template('home.html')
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 @app.route('/base')
 def base():
